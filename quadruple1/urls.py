47b68f8259e05d0105909c03ebdee11cdc9fa1ab
Vicky Qijing Zhang <vickyqijingzhang@gmail.com>
2015-06-12 17:57:38 -0500

    fix url problem

--- quadruple1/urls.py
+++ quadruple1/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^game/', include('game.urls')),
+    url(r'^$', include('game.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
